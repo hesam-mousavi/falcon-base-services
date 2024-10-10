fa7845dcaea76db001365c4550b1a993537b903a
Seyed Hesam Mousavi <hesam.mousavi@hotmail.com>
2024-10-10 22:15:03 +0330

    bugfix and add some data to first lines

--- falcon-base-services.php
+++ falcon-base-services.php
@@ -1,10 +1,39 @@
 <?php
 
 /**
- * Plugin Name: Falcon Base Services
- * Description: Most Used Base Services And Helper Functions For WordPress
- * Author: Seyed Hesam Mousavi
- * Version: 2.0.0
+ *  Plugin Name: Falcon Base Services
+ *  Description: Most Used Base Services And Helper Functions For WordPress
+ *  Version: 2.0.0
+ *  Author: Seyed Hesam Mousavi
+ *  License: MIT
+ *  License URI: https://opensource.org/licenses/MIT
+ *  Email: hesam.mousavi@hotmail.com
+ *
+ *  This plugin provides a set of base services and helper functions to enhance
+ *  WordPress development. It aims to bring service container-provider, Monolog, PHPMailer and some of the powerful
+ *  features of Laravel, such as Query Builder and Eloquent, Blade, Validation and ... into WordPress environment.
+ *
+ *  Features:
+ *  - Query Builder: Simplified database queries.
+ *  - Eloquent ORM: Object-relational mapping for better data handling.
+ *  - Blade-like Template Engine: Powerful templating capabilities.
+ *  - Monolog: Comprehensive logging for debugging and monitoring.
+ *  - PHPMailer: Easy and flexible email sending capabilities.
+ *  - Service Container and Service Provider: Efficient dependency injection and service management.
+ *  - Additional Helper Functions: Various utilities to speed up development.
+ *
+ *  Usage:
+ *  - After install in mu-plugins folder, the plugin will automatically load the necessary files and set up the
+ *    environment.
+ *  - Refer to the documentation for detailed usage examples and best practices.
+ *
+ *  Support:
+ *  - For support and contributions, please visit the GitHub repository:
+ *    https://github.com/hesam-mousavi/falcon-base-services
+ *  - Issues and pull requests are welcome.
+ *
+ * @package FalconBaseServices
+ * /
  */
 
 use FalconBaseServices\Start;
@@ -19,7 +48,7 @@ require_once __DIR__.'/vendor/autoload.php';
 // Determine if the application is in maintenance mode...
 if (\file_exists($maintenance = __DIR__.'/storage/maintenance.php')) {
     require $maintenance;
-    \die();
+    die();
 }
 
 \define('FALCON_BASE_SERVICE_PLUGIN_ROOT_DIR', plugin_dir_path(__FILE__));
