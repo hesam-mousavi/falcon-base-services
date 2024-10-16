81d09777e522b797aeed092a5f4bfdebf574cbf9
Seyed Hesam Mousavi <hesam.mousavi@hotmail.com>
2024-10-16 10:19:37 +0330

    add config func

--- app/functions.php
+++ app/functions.php
@@ -19,3 +19,26 @@ function falconEmail()
 {
     return FALCON_CONTAINER->get('email');
 }
+
+function config($file, $key = null, $folder_path = null)
+{
+    $folder = $folder_path ? rtrim($folder_path, '\\/').DIRECTORY_SEPARATOR : BASE_SERVICE_PLUGIN_CONFIG_DIR;
+    $file = $folder.$file.'.php';
+
+    if (file_exists($file)) {
+        $config = require_once $file;
+        if (!is_null($key)) {
+            if (array_key_exists($key, $config)) {
+                return $config[$key];
+            }
+
+            falconLogger()->warning("'$key' config key not exist in $file");
+            return null;
+        }
+
+        return $config;
+    }
+
+    falconLogger()->warning("$file config file not exist");
+    return null;
+}
