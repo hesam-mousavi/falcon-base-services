6004ea861f775283e6116c962e65d1d197992d97
Seyed Hesam Mousavi <hesam.mousavi@hotmail.com>
2025-07-02 11:47:23 +0330

    bugfix

--- app/Helper/Time.php
+++ app/Helper/Time.php
@@ -3,34 +3,76 @@
 namespace FalconBaseServices\Helper;
 
 use Carbon\Carbon;
+use DateTimeInterface;
+use DateTimeZone;
+
 
 class Time
 {
-    public static function translate($datetime, string $pattern = 'dd MMMM yyyy', string $locale = 'fa_IR', string $timezone = FALCON_BASE_TIME_ZONE): string
-    {
-        if (empty($datetime) || !$datetime instanceof \DateTimeInterface) {
-            $datetime = self::now();
-        }
-
+    public static function translate(
+        DateTimeInterface|string|int|null $datetime = null,
+        string $pattern = 'dd MMMM yyyy',
+        string $locale = null,
+        DateTimeZone|string|null $timezone = null,
+        int $calendar = \IntlDateFormatter::TRADITIONAL
+    ): string {
         try {
+            $timezone = self::normalizeTimezone($timezone);
+
+            if (empty($datetime)) {
+                $datetime = self::now($timezone);
+            }
+
+            if (is_int($datetime)) {
+                $datetime = (new \DateTimeImmutable('@' . $datetime))->setTimezone($timezone);
+            }
+
+            if (is_string($datetime)) {
+                $datetime = new \DateTime($datetime, $timezone);
+            }
+
+            if (!$datetime instanceof DateTimeInterface) {
+                $datetime = self::now($timezone);
+            }
+
             $formatter = new \IntlDateFormatter(
-                $locale,
+                $locale ?? get_locale(),
                 \IntlDateFormatter::FULL,
                 \IntlDateFormatter::FULL,
                 $timezone,
-                \IntlDateFormatter::GREGORIAN,
+                $calendar,
                 $pattern
             );
 
             return $formatter->format($datetime);
         } catch (\Exception $e) {
-            logger('Cant Translate Time');
+            logger('Can’t Translate Time: ' . $e->getMessage());
             return '';
         }
     }
 
-    public static function now(\DateTimeZone|string|null $time_zone = FALCON_BASE_TIME_ZONE): \DateTimeInterface
+
+    public static function now(DateTimeZone|string|null $timezone = null): DateTimeInterface
     {
-        return Carbon::now($time_zone);
+        $timezone = self::normalizeTimezone($timezone);
+        return Carbon::now($timezone);
+    }
+
+    protected static function normalizeTimezone(DateTimeZone|string|null $timezone): DateTimeZone
+    {
+        if (is_null($timezone)) {
+            return wp_timezone();
+        }
+
+        if ($timezone instanceof DateTimeZone) {
+            return $timezone;
+        }
+
+        try {
+            return new DateTimeZone($timezone);
+        } catch (\Exception $e) {
+            logger('Invalid timezone provided: ' . $timezone);
+            return wp_timezone();
+        }
     }
 }
