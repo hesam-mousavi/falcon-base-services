1bd5a4cf43b79b382af87871dc0fa2f8655801bb
Seyed Hesam Mousavi <hesam.mousavi@hotmail.com>
2025-07-02 11:44:37 +0330

    remove unnecessary funcs

--- app/Start.php
+++ app/Start.php
@@ -6,24 +6,7 @@ class Start
 {
     public function run(): void
     {
-        add_action('init', [$this, 'init']);
-    }
-
-    public function init(): void
-    {
-        $this->routes();
-        $this->actions();
-        $this->filters();
-    }
-
-    public function routes(): void
-    {
         $routes = new Routes();
         $routes->register();
     }
-
-    public function actions() {}
-
-    public function filters() {}
-
 }
