243e54f4a069835bc284da53bd33893b7aaa5a13
Seyed Hesam Mousavi <hesam.mousavi@hotmail.com>
2025-07-02 11:48:04 +0330

    commented providers that not required

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     \FalconBaseServices\Providers\LoggerServiceProvider::class,
     \FalconBaseServices\Providers\DBServiceProvider::class,
-    \FalconBaseServices\Providers\TemplateServiceProvider::class,
-    \FalconBaseServices\Providers\EmailServiceProvider::class,
+//    \FalconBaseServices\Providers\TemplateServiceProvider::class,
+//    \FalconBaseServices\Providers\EmailServiceProvider::class,
 ];
